b38a433f9bbfa2fe3a7296855473138dd1e02bcb
srbhjdhv28 <44156672+srbhjdhv28@users.noreply.github.com>
2019-01-21 21:31:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ NewsFeed = require('./api/models/newsModel');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost/myDB');
-mongoose.connect('mongodb://SaurabhJ:srbh_jdhv28@ds159641.mlab.com:59641/mydb');
+mongoose.connect('mongodb://srbhjdhv:srbhjdhv28@ds159641.mlab.com:59641/mydb');
 var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
